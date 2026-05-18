f54dc84de8e263b8963ebf60d40133d2b9ecf204
Josekariz <sejokarizz@gmail.com>
2026-05-18 12:59:41 +0300

    update global CSS variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 const page = () => {
-  return <h1 className="text-red-800">Hi there!</h1>;
+  return (
+    <section>
+      <h1 className="text-center">Welcome to Devent</h1>
+      <p className="text-center mt-5">All your Meetups all in one place</p>
+    </section>
+  );
 };
 
 export default page;
