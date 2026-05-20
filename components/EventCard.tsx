9f89fa89159979bb889f90785e487a2d9d8f1ac2
Josekariz <sejokarizz@gmail.com>
2026-05-20 19:43:35 +0300

    update EventCard component to include location, date, and time props

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -4,10 +4,21 @@ import Link from "next/link";
 interface EventCardProps {
   title: string;
   image: string;
+  slug: string;
+  location: string;
+  date: string;
+  time: string;
 }
-const EventCard = ({ title, image }: EventCardProps) => {
+const EventCard = ({
+  title,
+  image,
+  slug,
+  location,
+  date,
+  time,
+}: EventCardProps) => {
   return (
-    <Link href={`/events`}>
+    <Link href={`/events/${slug}`} id="event-card">
       <Image
         src={image}
         alt={title}
@@ -15,7 +26,22 @@ const EventCard = ({ title, image }: EventCardProps) => {
         width={410}
         className="poster"
       />
+      <div className="flex flex-row gap-2">
+        <Image src="/icons/pin.svg" alt="Location" width={14} height={14} />
+        <p>{location}</p>
+      </div>
       <p className="title">{title}</p>
+      <div className="datetime">
+        <div>
+          <Image src="/icons/calendar.svg" alt="date" width={14} height={14} />
+          <p>{date}</p>
+        </div>
+
+        <div>
+          <Image src="/icons/clock.svg" alt="time" width={14} height={14} />
+          <p>{time}</p>
+        </div>
+      </div>
     </Link>
   );
 };
